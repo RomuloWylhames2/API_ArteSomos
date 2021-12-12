078357147b26fd4a5b2fe77a69cac0713b5eb3ee
Michel Domingos <micheldslive@gmail.com>
2021-12-12 15:42:37 -0300

    first commit

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -2,10 +2,10 @@ export default {
   development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "artesomos",
+      host: "us-cdbr-east-05.cleardb.net",
+      user: "b12bfac026369e",
+      password: "c883be1f",
+      database: "heroku_2f79222f89e622b",
     },
   },
 };
